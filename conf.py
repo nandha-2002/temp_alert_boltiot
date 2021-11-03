03be91d3dfae94ea1fa6a4d4131225c865ed1a39
nandha-2002 <93634342+nandha-2002@users.noreply.github.com>
2021-11-03 11:17:33 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 """Configurations for telegram_alert.py"""
-bolt_api_key = "8f5bc5a1-a969-4991-91c0-c762433556b7"
-device_id = "BOLT13168786"
-telegram_chat_id = "@temp_alert_nandha"
-telegram_bot_id = "bot2090451021:AAGEP-hNwLXnzqYFEP3PjrRuLNzuO9l1_5U"
+bolt_api_key = "enter the api key"
+device_id = "enter device id"
+telegram_chat_id = "@chat id"
+telegram_bot_id = "bot enter api token "
 threshold = 250
