117620f8a3fd48f3c322afab9c49456042602b53
nandha-2002 <93634342+nandha-2002@users.noreply.github.com>
2021-11-03 11:13:05 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,6 @@
+"""Configurations for telegram_alert.py"""
+bolt_api_key = "8f5bc5a1-a969-4991-91c0-c762433556b7"
+device_id = "BOLT13168786"
+telegram_chat_id = "@temp_alert_nandha"
+telegram_bot_id = "bot2090451021:AAGEP-hNwLXnzqYFEP3PjrRuLNzuO9l1_5U"
+threshold = 250
